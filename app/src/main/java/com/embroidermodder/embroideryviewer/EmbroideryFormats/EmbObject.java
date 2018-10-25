6c2e206fd3e1e8cc094da5abbc857631d606285e
tatarize <tatarize@gmail.com>
2018-10-25 01:28:56 -0700

    Delete EmbObject.java

--- app/src/main/java/com/embroidermodder/embroideryviewer/EmbroideryFormats/EmbObject.java
+++
@@ -1,9 +0,0 @@
-package com.embroidermodder.embroideryviewer.EmbroideryFormats;
-
-import com.embroidermodder.embroideryviewer.geom.Points;
-
-public interface EmbObject {
-    EmmThread getThread();
-    Points getPoints();
-    int getType();
-}
